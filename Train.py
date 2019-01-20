d125e870aacfb48d772a8b3e0402456a8272d0bf
AerostatDrones <34020962+AerostatDrones@users.noreply.github.com>
2019-01-20 12:24:59 +0530

    Update Train.py

--- Train.py
+++ Train.py
@@ -4,19 +4,19 @@ mlp = MicroMLP.Create( neuronsByLayers           = [2, 3, 2],
                        activationFuncName        = MicroMLP.ACTFUNC_SIGMOID,
                        layersAutoConnectFunction = MicroMLP.LayersFullConnect )
 
-false  = MicroMLP.NNValue(0,1,0) 
+false  = MicroMLP.NNValue(0,1,0) #(Min , Max, Value)
 true   = MicroMLP.NNValue(0,1,1)
 
 motor_right = mlp.NNValue(0,100,0) # 0 for CW Rotation
 motor_left  = mlp.NNValue(0,100,100) # 100 for CCW Rotation
-stop = mlp.NNValue.FromPercent(51) # 50 stops servo Rotation
+stop = mlp.NNValue.FromPercent(50.7) # 50 stops servo Rotation
 
-mlp.AddExample( [false, false], [motor_right, motor_left] )
+mlp.AddExample( [false, false], [motor_right, motor_left] ) # ([Input] , [OutPut]) as vector values
 mlp.AddExample( [false, true ], [motor_right, stop ] )
 mlp.AddExample( [true , true], [stop, stop] )
 mlp.AddExample( [true , false], [stop, motor_left] )
 
-learnCount = mlp.LearnExamples(maxSeconds= 0xffffffffffffffff)
+learnCount = mlp.LearnExamples(maxSeconds= 0xffffffffffffffff) # Max time to learn, default 30 seconds
 
 print( "LEARNED :" )
 print( "  - False and False = %s , %s" , mlp.Predict([false, false])[0].AsPercent, mlp.Predict([false, false])[1].AsPercent)
